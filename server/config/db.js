b541f82b642ad66987885984577b3a6c8f7bba6b
Cyrus Chau <hung940801@live.hk>
2023-03-04 01:56:51 -0500

    init commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://midterm229:midterm229_Admin@cluster0.2oqaxwf.mongodb.net/books229?retryWrites=true&w=majority"
+};
